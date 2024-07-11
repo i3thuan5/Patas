1d69e07f64b9321c8c8236fc9c35ea69face5ae4
Asing i Mac M1 16G <hong@ithuan.tw>
2024-07-11 16:21:32 +0800

    加錄音編號的檢查。

--- truku.py
+++ truku.py
@@ -7,15 +7,19 @@ from argparse import ArgumentParser
 def xlsx轉錄音稿(xlsx檔名):
     語料名 = 找語料名(xlsx檔名)
     結果 = []
+    錄音編號 = None
     for 篇名, dataframe in pandas.read_excel(
         xlsx檔名, engine='openpyxl',
         sheet_name=None,
     ).items():
         結果.append(f"【{語料名}-{篇名}】")
-        for tsua in dataframe.fillna('').itertuples():
-            結果.append(f'{tsua.錄音編號}')
-            結果.append(tsua.太魯閣語.strip())
+        for 行 in dataframe.fillna('').itertuples():
+            if 錄音編號 is not None and 行.錄音編號 != 錄音編號 + 1:
+                raise ValueError(f"{篇名}裡的錄音編號{行.錄音編號}應該要是{錄音編號+1}")
+            結果.append(f'{行.錄音編號}')
+            結果.append(行.太魯閣語.strip())
             結果.append('')
+            錄音編號 = 行.錄音編號
     return 結果[:-1]
 
 
