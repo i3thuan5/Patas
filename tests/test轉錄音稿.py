03db626e87b9ff0a8011df1680e04eb10a6b9964
Tia i X570-ACE <hong@ithuan.tw>
2024-07-11 10:52:06 +0800

    檔名另外指令處理。

--- tests/test轉錄音稿.py
+++ tests/test轉錄音稿.py
@@ -7,10 +7,10 @@ class xlsx轉錄音稿試驗(TestCase):
     def test格式檢查(self):
         self.maxDiff = None
         xlsx檔名 = join(
-            abspath(dirname(__file__)), 'patas', '1-4｜D-TL01-005.xlsx'
+            abspath(dirname(__file__)), '格式檢查', '1-4｜D-TL01-005.xlsx'
         )
         錄音稿檔名 = join(
-            abspath(dirname(__file__)), 'patas', '1-4｜D-TL01-005.txt'
+            abspath(dirname(__file__)), '格式檢查', '1-4｜D-TL01-005.txt'
         )
         結果 = xlsx轉錄音稿(xlsx檔名)
         答案 = []
