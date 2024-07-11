c953ff14f634cecad219e56dba5a844903655f0c
Tia i X570-ACE <hong@ithuan.tw>
2024-07-11 09:58:33 +0800

    語料名獨立試驗。

--- tests/test轉錄音稿.py
+++ tests/test轉錄音稿.py
@@ -1,15 +1,15 @@
 from unittest import TestCase
-from os.path import join, dirname
+from os.path import join, abspath, dirname
 from truku import xlsx轉錄音稿
 
 class xlsx轉錄音稿試驗(TestCase):
     def test格式檢查(self):
         self.maxDiff = None
         xlsx檔名 = join(
-            dirname(__file__), '格式檢查', '1-4｜D-TL01-005.xlsx'
+            abspath(dirname(__file__)), '格式檢查', '1-4｜D-TL01-005.xlsx'
         )
         錄音稿檔名 = join(
-            dirname(__file__), '格式檢查', '1-4｜D-TL01-005.txt'
+            abspath(dirname(__file__)), '格式檢查', '1-4｜D-TL01-005.txt'
         )
         結果 = xlsx轉錄音稿(xlsx檔名)
         答案 = []
--- truku.py
+++ truku.py
@@ -1,17 +1,21 @@
 import pandas
+import re
+from os.path import basename
 
 def xlsx轉錄音稿(xlsx檔名):
+	xlsx語料名 = 語料名(xlsx檔名)
 	結果 = []
 	for 篇名, dataframe in pandas.read_excel(
 		xlsx檔名, engine='openpyxl',
-		sheet_name=None
+		sheet_name=None,
 	).items():
-		print(篇名, dataframe)
-		結果.append(f"【{篇名}】")
+		結果.append(f"【{語料名}-{篇名}】")
 		for tsua in dataframe.fillna('').itertuples():
-			print('tsua', tsua)
 			結果.append(f'{tsua.錄音編號}')
 			結果.append(tsua.太魯閣語.strip())
 			結果.append(tsua.華語.strip())
 			結果.append('')
 	return 結果[:-1]
+
+def 語料名(xlsx檔名):
+	return re.search(r'D-[STP][LVTR]\d\d-\d\d\d', basename(xlsx檔名)).group(0)
