7a8dca777c07cdfafa07320e97bbe599308fa94f
Tia i X570-ACE <hong@ithuan.tw>
2024-07-11 10:44:15 +0800

    配合travis CI 指令A-Z

--- tests/test語料名.py
+++ tests/test語料名.py
@@ -6,6 +6,6 @@ from truku import 找語料名
 class 語料名試驗(TestCase):
     def test格式檢查(self):
         xlsx檔名 = join(
-            abspath(dirname(__file__)), '格式檢查', '1-4｜D-TL01-005.xlsx'
+            abspath(dirname(__file__)), 'patas', '1-4｜D-TL01-005.xlsx'
         )
         self.assertEqual(找語料名(xlsx檔名), 'D-TL01-005', xlsx檔名)
