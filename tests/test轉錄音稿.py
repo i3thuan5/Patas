f11461c63d2d3c433c092cccb3db0c4bcd1226e5
Tia i X570-ACE <hong@ithuan.tw>
2024-07-11 09:25:48 +0800

    加test

---
+++ tests/test轉錄音稿.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+
+class xlsx轉錄音稿試驗(TestCase):
+    def test格式檢查(self):
+        xlsx檔名 = join(
+            dirname(__file__), '格式檢查', '1-4｜D-TL01-005.xlsx'
+        )
+        錄音稿檔名 = join(
+            dirname(__file__), '格式檢查', '1-4｜D-TL01-005.txt'
+        )
+        結果 = xlsx轉錄音稿(xlsx檔名)
+        with open(錄音稿檔名) as 檔案:
+            self.assertEqual(結果, 檔案.read())
