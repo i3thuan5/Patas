67224ed8d951f221d34d6dde67023feabb48e35e
Asing i Mac M1 16G <hong@ithuan.tw>
2024-08-27 14:46:09 +0800

    Misakamoko'

--- tests/testPangcah.py
+++ tests/testPangcah.py
@@ -7,7 +7,8 @@ from pangcah import xlsx轉錄音稿
 class 語料名試驗(TestCase):
     def test格式檢查(self):
         xlsx檔名 = join(
-            abspath(dirname(__file__)), '格式檢查', 'D-PV01｜1~353 (C-PL002)sample.xlsx'
+            abspath(dirname(__file__)),
+            '格式檢查', 'D-PV01｜1~353 (C-PL002)sample.xlsx'
         )
         self.assertEqual(找語料名(xlsx檔名), 'D-PV01', xlsx檔名)
 
@@ -16,10 +17,12 @@ class xlsx轉錄音稿試驗(TestCase):
     def test格式檢查(self):
         self.maxDiff = None
         xlsx檔名 = join(
-            abspath(dirname(__file__)), '格式檢查', 'D-PV01｜1~353 (C-PL002)sample.xlsx'
+            abspath(dirname(__file__)),
+            '格式檢查', 'D-PV01｜1~353 (C-PL002)sample.xlsx'
         )
         錄音稿檔名 = join(
-            abspath(dirname(__file__)), '格式檢查', 'D-PV01｜1~353 (C-PL002)sample.txt'
+            abspath(dirname(__file__)),
+            '格式檢查', 'D-PV01｜1~353 (C-PL002)sample.txt'
         )
         結果 = xlsx轉錄音稿(xlsx檔名)
         答案 = []
