e05fbc5b5a6d47098d310f732b9f81ba9cf23eaa
Tia i X570-ACE <hong@ithuan.tw>
2024-12-18 13:56:49 +0800

    修test。

--- integrationTestPrepration.sh
+++ integrationTestPrepration.sh
@@ -2,4 +2,4 @@
 
 cp tests/格式檢查/1-4｜D-TL01-005.xlsx D-TL01-005.xlsx
 cp 'tests/格式檢查/D-PV01｜1~353 (C-PL002)sample.xlsx' D-PV01.xlsx
-cp tests/格式檢查/D-SL07-001｜合成音稿Sample.txt D-SL07-001.txt
+cp tests/格式檢查/D-SL07-001｜合成音稿Sample.xlsx D-SL07-001.xlsx
