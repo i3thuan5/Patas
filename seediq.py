8b1f75c9ead10c1a5d0faa9974996055a254a2c6
Asing i Mac M1 16G <hong@ithuan.tw>
2024-12-18 14:14:27 +0800

    欄位名都統一叫做Tgdaya。

--- seediq.py
+++ seediq.py
@@ -17,7 +17,7 @@ def xlsx轉錄音稿(xlsx檔名):
             if 錄音編號 is not None and 行.錄音編號 != 錄音編號 + 1:
                 raise ValueError(f"「{篇名}」裡的錄音編號{行.錄音編號}應該要是{錄音編號+1}")
             結果.append(f'{行.錄音編號}')
-            結果.append(getattr(行, 'Seediq Tgdaya 語').strip())
+            結果.append(getattr(行, 'Tgdaya').strip())
             結果.append('')
             錄音編號 = 行.錄音編號
     return 結果[:-1]
